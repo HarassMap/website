cd0ad3324243d3139c16f635f741ed25e85232ce
Paul Barton <info@thisispaul.co.uk>
2017-05-18 11:31:40 +0100

    Only showing the content blocks and domains for that user

--- plugins/harassmap/domain/controllers/Content.php
+++ plugins/harassmap/domain/controllers/Content.php
@@ -3,12 +3,13 @@
 namespace Harassmap\Domain\Controllers;
 
 use Backend\Classes\Controller;
+use BackendAuth;
 use BackendMenu;
 
 class Content extends Controller
 {
-    public $implement = ['Backend\Behaviors\ListController','Backend\Behaviors\FormController'];
-    
+    public $implement = ['Backend\Behaviors\ListController', 'Backend\Behaviors\FormController'];
+
     public $listConfig = 'config_list.yaml';
     public $formConfig = 'config_form.yaml';
 
@@ -16,5 +17,24 @@ class Content extends Controller
     {
         parent::__construct();
         BackendMenu::setContext('Harassmap.Domain', 'harassmap.domain', 'harassmap.domain.content');
+    }
+
+    public function listExtendQuery($query)
+    {
+        $user = BackendAuth::getUser();
+
+        // if the user is a super use then stop here
+        if ($user->isSuperUser()) {
+            return;
+        }
+
+        // TODO: Only do the domain check on certain user groups
+
+        $domains = $user->domains;
+
+        foreach ($domains as $domain) {
+            $query->orWhere('domain_id', '=', $domain->id);
+        }
+
     }
 }
--- plugins/harassmap/domain/controllers/Domain.php
+++ plugins/harassmap/domain/controllers/Domain.php
@@ -1,12 +1,13 @@
 <?php namespace Harassmap\Domain\Controllers;
 
 use Backend\Classes\Controller;
+use BackendAuth;
 use BackendMenu;
 
 class Domain extends Controller
 {
-    public $implement = ['Backend\Behaviors\ListController','Backend\Behaviors\FormController'];
-    
+    public $implement = ['Backend\Behaviors\ListController', 'Backend\Behaviors\FormController'];
+
     public $listConfig = 'config_list.yaml';
     public $formConfig = 'config_form.yaml';
 
@@ -14,5 +15,24 @@ class Domain extends Controller
     {
         parent::__construct();
         BackendMenu::setContext('Harassmap.Domain', 'harassmap.domain', 'harassmap.domain.domain');
+    }
+
+    public function listExtendQuery($query)
+    {
+        $user = BackendAuth::getUser();
+
+        // if the user is a super use then stop here
+        if ($user->isSuperUser()) {
+            return;
+        }
+
+        // TODO: Only do the domain check on certain user groups
+
+        $domains = $user->domains;
+
+        foreach ($domains as $domain) {
+            $query->orWhere('id', '=', $domain->id);
+        }
+
     }
 }
