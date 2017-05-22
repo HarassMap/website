84dbd66185f463fb3a5423ee12e511f349223521
Paul Barton <info@thisispaul.co.uk>
2017-05-22 14:26:24 +0100

    Adding domain permission checks to the tip admin

--- plugins/harassmap/domain/controllers/Tip.php
+++ plugins/harassmap/domain/controllers/Tip.php
@@ -1,12 +1,15 @@
 <?php namespace Harassmap\Domain\Controllers;
 
 use Backend\Classes\Controller;
+use BackendAuth;
 use BackendMenu;
+use Harassmap\Domain\Models\Tip as TipModel;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class Tip extends Controller
 {
-    public $implement = ['Backend\Behaviors\ListController','Backend\Behaviors\FormController'];
-    
+    public $implement = ['Backend\Behaviors\ListController', 'Backend\Behaviors\FormController'];
+
     public $listConfig = 'config_list.yaml';
     public $formConfig = 'config_form.yaml';
 
@@ -14,5 +17,51 @@ class Tip extends Controller
     {
         parent::__construct();
         BackendMenu::setContext('Harassmap.Domain', 'harassmap.domain', 'harassmap.domain.tips');
+    }
+
+    public function listExtendQuery($query)
+    {
+        $user = BackendAuth::getUser();
+
+        // if the user is a super use then stop here
+        if ($user->isSuperUser()) {
+            return;
+        }
+
+        // TODO: Only do the domain check on certain user groups
+
+        $domains = $user->domains;
+
+        foreach ($domains as $domain) {
+            $query->orWhere('domain_id', '=', $domain->id);
+        }
+
+    }
+
+    public function update($recordId, $context = null)
+    {
+        $user = BackendAuth::getUser();
+
+        // if the user is a super use then stop here
+        if (!$user->isSuperUser()) {
+
+            $content = TipModel::find($recordId);
+            $id = $content->domain->id;
+            $domains = $user->domains;
+            $found = false;
+
+            foreach ($domains as $domain) {
+                if ($domain->id === $id) {
+                    $found = true;
+                    break;
+                }
+            }
+
+            if (!$found) {
+                throw new AccessDeniedHttpException();
+            }
+        }
+
+        return $this->asExtension('FormController')->update($recordId, $context);
     }
 }
