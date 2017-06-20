06f636aa07ea0a256bf2464ff2bd45c32fabf470
Paul Barton <info@thisispaul.co.uk>
2017-06-20 17:29:05 +0100

    Adding default country to the incident report

--- plugins/harassmap/incidents/components/ReportIncident.php
+++ plugins/harassmap/incidents/components/ReportIncident.php
@@ -3,6 +3,8 @@
 namespace Harassmap\Incidents\Components;
 
 use Cms\Classes\ComponentBase;
+use Harassmap\Domain\Models\Domain;
+use Harassmap\Incidents\Models\Country;
 
 class ReportIncident extends ComponentBase
 {
@@ -17,7 +19,11 @@ class ReportIncident extends ComponentBase
 
     public function onRender()
     {
+        $this->page['countries'] = Country::all()->lists('name', 'id');
 
+        $domain = Domain::getBestMatchingDomain();
+
+        $this->page['default_country'] = $domain->country_id;
     }
 
     public function onSubmit()
