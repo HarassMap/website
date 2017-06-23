baf07918d1eeca3d818981a54924234a3c1aa8ac
Paul Barton <info@thisispaul.co.uk>
2017-06-23 15:10:03 +0100

    Adding incident controller

---
+++ plugins/harassmap/incidents/controllers/Incidents.php
@@ -0,0 +1,18 @@
+<?php namespace Harassmap\Incidents\Controllers;
+
+use Backend\Classes\Controller;
+use BackendMenu;
+
+class Incidents extends Controller
+{
+    public $implement = ['Backend\Behaviors\ListController','Backend\Behaviors\FormController'];
+    
+    public $listConfig = 'config_list.yaml';
+    public $formConfig = 'config_form.yaml';
+
+    public function __construct()
+    {
+        parent::__construct();
+        BackendMenu::setContext('Harassmap.Incidents', 'harassmap.incidents', 'harassmap.incidents.incidents');
+    }
+}
