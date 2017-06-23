01349ddcd8d34adfe3c7df6fad73ee9fd08db6e9
Paul Barton <info@thisispaul.co.uk>
2017-06-23 15:51:32 +0100

    Adding assistance seed data

---
+++ plugins/harassmap/incidents/updates/seed_harassmap_incidents_assistance_table.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Harassmap\Incidents\Updates;
+
+use Harassmap\Incidents\Models\Assistance;
+use Seeder;
+use Symfony\Component\Yaml\Yaml;
+
+class SeedHarassmapAssistanceRoleTable extends Seeder
+{
+    public function run()
+    {
+        $filename = __DIR__ . '/data/assistance.yml';
+        $fixtures = Yaml::parse(file_get_contents($filename));
+
+        $assistanceData = $fixtures['types'];
+
+        foreach ($assistanceData as $key => $typeData) {
+            Assistance::create([
+                'title' => $typeData['title']
+            ]);
+        }
+    }
+}
