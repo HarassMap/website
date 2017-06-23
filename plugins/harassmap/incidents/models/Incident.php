bd3bab56cb17859eda8cffd5b69cd3ee300147a4
Paul Barton <info@thisispaul.co.uk>
2017-06-23 15:29:08 +0100

    Formatting

--- plugins/harassmap/incidents/models/Incident.php
+++ plugins/harassmap/incidents/models/Incident.php
@@ -55,10 +55,5 @@ class Incident extends Model
         'role' => Role::class,
     ];
 
-    public $belongsToMany = [
-        'categories' => [
-            Category::class,
-            'table' => 'harassmap_incidents_incident_category'
-        ]
-    ];
+    public $belongsToMany = ['categories' => [Category::class, 'table' => 'harassmap_incidents_incident_category']];
 }
