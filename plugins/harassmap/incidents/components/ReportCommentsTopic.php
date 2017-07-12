e4bdccdddda54d852bd73f2733780772b949a58f
Paul Barton <info@thisispaul.co.uk>
2017-07-12 14:06:43 +0100

    Removing comment quote

--- plugins/harassmap/incidents/components/ReportCommentsTopic.php
+++ plugins/harassmap/incidents/components/ReportCommentsTopic.php
@@ -15,4 +15,15 @@ class ReportCommentsTopic extends Topic
         ];
     }
 
+    public function onRun()
+    {
+        $this->prepareVars();
+        $this->page['channel'] = $this->getChannel();
+        $this->page['topic']   = $topic = $this->getTopic();
+        $this->page['member']  = $member = $this->getMember();
+        $this->handleOptOutLinks();
+
+        return $this->preparePostList();
+    }
+
 }
