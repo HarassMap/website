ecf4769f08a0822a2989be99c6b66ba6800ab75d
Paul Barton <info@thisispaul.co.uk>
2017-07-18 15:21:01 +0100

    Adding the info window with the correct info

--- themes/harassmap/assets/src/map/map.home.js
+++ themes/harassmap/assets/src/map/map.home.js
@@ -2,7 +2,9 @@
 
 import debounce from "debounce";
 import _ from "lodash";
+import moment from "moment";
 import mapStyle from "./map.style.json";
+import Handlebars from "handlebars";
 
 export class HomePageMap {
 
@@ -11,6 +13,9 @@ export class HomePageMap {
             lng = element.dataset.lng,
             zoom = element.dataset.zoom,
             centre = new google.maps.LatLng(lat, lng);
+
+        // get the link for the read more
+        this.link = element.dataset.link;
 
         this._element = element;
 
@@ -28,6 +33,7 @@ export class HomePageMap {
         });
 
         this.markers = [];
+        this.windows = [];
 
         google.maps.event.addListener(this.map, 'bounds_changed', debounce(() => {
             this.getReports();
@@ -63,9 +69,18 @@ export class HomePageMap {
     addMarker(report) {
         let centre = new google.maps.LatLng(report.location.lat, report.location.lng);
         let icon = '/themes/harassmap/assets/img/map/' + (report.intervention ? 'intervention' : 'incident') + '.svg';
+        let date = moment(report.date);
+        let location = report.location;
+        let address = location.address + ', ' + location.city + ', ' + location.region;
+        let source   = $("#info-template").html();
+        let template = Handlebars.compile(source);
 
         let infowindow = new google.maps.InfoWindow({
-            content: '<div class="infowindow">THIS IS A TEST</div>'
+            content: template({
+                time: date.format("L, LT"),
+                address: address,
+                link: this.link.replace('REPORT_ID', report.public_id)
+            })
         });
 
         let marker = new google.maps.Marker({
@@ -75,10 +90,18 @@ export class HomePageMap {
         });
 
         marker.addListener('click', () => {
+            this.closeWindows();
             infowindow.open(this.map, marker);
         });
 
+        this.windows.push(infowindow);
         this.markers.push(marker);
+    }
+
+    closeWindows() {
+        _.forEach(this.windows, (window) => {
+            window.close();
+        });
     }
 
     clearMarkers() {
@@ -86,6 +109,7 @@ export class HomePageMap {
             marker.setMap(null);
         });
 
+        this.windows = [];
         this.markers = [];
     }
 
