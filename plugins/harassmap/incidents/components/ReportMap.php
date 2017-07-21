76d0e2ac2ad2b7b6f189d00395806662177bed68
Paul Barton <info@thisispaul.co.uk>
2017-07-21 11:05:22 +0100

    Adding the table onto the home page

--- plugins/harassmap/incidents/components/ReportMap.php
+++ plugins/harassmap/incidents/components/ReportMap.php
@@ -3,7 +3,7 @@
 namespace Harassmap\Incidents\Components;
 
 use Cms\Classes\ComponentBase;
-use Harassmap\Incidents\Models\Domain;
+use Cms\Classes\Page;
 use Harassmap\Incidents\Models\Incident;
 
 class ReportMap extends ComponentBase
@@ -17,6 +17,30 @@ class ReportMap extends ComponentBase
         ];
     }
 
+    public function defineProperties()
+    {
+        return [
+            'viewPage' => [
+                'title' => 'harassmap.incidents::lang.tip.list.page_name',
+                'description' => 'harassmap.incidents::lang.tip.list.page_help',
+                'type' => 'dropdown',
+                'group' => 'Links',
+            ],
+        ];
+    }
+
+    public function getViewPageOptions()
+    {
+        return Page::sortBy('baseFileName')->lists('baseFileName', 'baseFileName');
+    }
+
+    public function onRender()
+    {
+        $this->page['reports'] = Incident::orderBy('date', 'desc')->limit(5)->get();
+
+        $this->page['viewPage'] = $this->property('viewPage');
+    }
+
     public function onGetReports()
     {
         $data = post();
@@ -25,7 +49,7 @@ class ReportMap extends ComponentBase
         $reports = [];
 
         // if the bounds exist
-        if(array_key_exists('bounds', $data)) {
+        if (array_key_exists('bounds', $data)) {
             $reports = Incident::whereInsideBounds($data['bounds']);
         }
 
