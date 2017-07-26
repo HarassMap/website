1955adff2e505909ef7f19d466442375cff47f82
Paul Barton <info@thisispaul.co.uk>
2017-07-26 11:42:34 +0100

    Removing the filter for now

--- themes/harassmap/assets/src/main.js
+++ themes/harassmap/assets/src/main.js
@@ -1,11 +1,9 @@
 'use strict';
 
 import MapFactory from "./map/map.factory";
-import { initCitySelector } from "./utils/citySelector";
-import screenfull from 'screenfull';
+import { initBanner } from './utils/banner';
 
 import { createDatePicker } from "./utils/datePicker";
-import { emitter, REFRESH_MAP, FILTER_MAP } from './utils/events';
 import { initGeolocate } from "./utils/geoLocate";
 import { createTimePicker } from "./utils/timePicker";
 import { initToggleIntervention } from "./utils/toggleIntervention";
@@ -17,70 +15,13 @@ window.initMap = (id = 'map') => {
     MapFactory.createFromElement(document.getElementById(id));
 };
 
+window.initBanner = initBanner;
+
 window.initReportIncidentPage = () => {
     createDatePicker('date');
     createTimePicker('time');
-    // initCitySelector();
     initGeolocate();
     initToggleIntervention();
-};
-
-window.initBanner = () => {
-    let $banners = $('.banner'),
-        $controls = $('.control');
-
-    initFullscreenMap();
-    initMapFilter();
-
-    $banners.not('.active').hide();
-
-    $controls.on('click', function (event) {
-        let $control = $(this),
-            href = $control.attr('href');
-
-        event.preventDefault();
-
-        $controls.removeClass('active');
-        $control.addClass('active');
-
-        $banners.hide();
-        $(href).show();
-
-        emitter.emit(REFRESH_MAP);
-    });
-};
-
-const initMapFilter = () => {
-  let $filterButton = $('#map-filter')  ,
-      $filter = $('#filter');
-
-    $filterButton.on('click', (event) => {
-        event.preventDefault();
-
-        $filter.toggle();
-        $filterButton.toggleClass('active');
-    });
-};
-
-const initFullscreenMap = () => {
-    let $map = $('#report-map');
-
-    screenfull.on('change', () => {
-
-        if(screenfull.isFullscreen) {
-            $map.addClass('full-screen');
-        } else {
-            $map.removeClass('full-screen');
-        }
-    });
-
-    $('#map-fullscreen').on('click', (event) => {
-        event.preventDefault();
-
-        if (screenfull.enabled) {
-            screenfull.toggle($map[0]);
-        }
-    });
 };
 
 $('.row-link').each((index, element) => {
---
+++ themes/harassmap/assets/src/utils/banner.js
@@ -0,0 +1,62 @@
+'use strict';
+
+import screenfull from 'screenfull';
+import { emitter, REFRESH_MAP } from './events';
+
+export const initBanner = () => {
+    let $banners = $('.banner'),
+        $controls = $('.control');
+
+    initFullscreenMap();
+    // initMapFilter();
+
+    $banners.not('.active').hide();
+
+    $controls.on('click', function (event) {
+        let $control = $(this),
+            href = $control.attr('href');
+
+        event.preventDefault();
+
+        $controls.removeClass('active');
+        $control.addClass('active');
+
+        $banners.hide();
+        $(href).show();
+
+        emitter.emit(REFRESH_MAP);
+    });
+};
+
+const initMapFilter = () => {
+    let $filterButton = $('#map-filter'),
+        $filter = $('#filter');
+
+    $filterButton.on('click', (event) => {
+        event.preventDefault();
+
+        $filter.toggle();
+        $filterButton.toggleClass('active');
+    });
+};
+
+const initFullscreenMap = () => {
+    let $map = $('#report-map');
+
+    screenfull.on('change', () => {
+
+        if (screenfull.isFullscreen) {
+            $map.addClass('full-screen');
+        } else {
+            $map.removeClass('full-screen');
+        }
+    });
+
+    $('#map-fullscreen').on('click', (event) => {
+        event.preventDefault();
+
+        if (screenfull.enabled) {
+            screenfull.toggle($map[0]);
+        }
+    });
+};
