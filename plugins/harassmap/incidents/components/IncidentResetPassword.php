8df13bb8c00d37c0a5d351260567ebee09c32186
Paul Barton <info@thisispaul.co.uk>
2017-08-08 19:20:36 +0100

    Checking that the password reset token is correct

--- plugins/harassmap/incidents/components/IncidentResetPassword.php
+++ plugins/harassmap/incidents/components/IncidentResetPassword.php
@@ -4,9 +4,24 @@ namespace Harassmap\Incidents\Components;
 
 use Exception;
 use RainLab\User\Components\ResetPassword;
+use RainLab\User\Models\User;
 
 class IncidentResetPassword extends ResetPassword
 {
+
+    public function onRender()
+    {
+        $code = $this->code();
+
+        if ($code) {
+            $user = User::where('reset_password_code', '=', $code)->first();
+
+            // if there is no user then error
+            if (is_null($user)) {
+                $this->page['error'] = true;
+            }
+        }
+    }
 
     public function onRestorePassword()
     {
