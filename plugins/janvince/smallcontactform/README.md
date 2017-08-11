7f97b8ebd37db1b22126068e73e383f652937375
Paul Barton <info@thisispaul.co.uk>
2017-08-11 14:15:08 +0100

    Adding small contact form

---
+++ plugins/janvince/smallcontactform/README.md
@@ -0,0 +1,215 @@
+# Small Contact form
+> Simple but flexible contact form builder with custom fields, validation and passive antispam.
+
+
+## Installation
+
+**GitHub** clone into `/plugins` dir:
+
+```sh
+git clone https://github.com/jan-vince/smallcontactform
+```
+
+**OctoberCMS backend**
+
+Just look for 'Small Contact Form' in search field in:
+> Settings > Updates & Plugins > Install plugins
+
+### Permissions
+
+> Settings > Administrators
+
+You can set permissions to restrict access to *Settings > Small plugins > Contact form* and to messages list.
+
+
+## Setup new Contact form
+
+> Settings > Small Contact form
+
+### FORM
+
+* You can set your own CSS class name and general success/error messages.
+* If you need it, placeholders can be used instead of labels
+* Form can be hidden after successful submit.
+
+
+#### Enable AJAX
+
+By default, sending form will trigger page reload. With AJAX, everything can be done without page reloading which will be more user friendly.    
+*If user's browser doesn't support (or has disabled) JavaScript, form will still work with page reloads after send.*
+
+* For AJAX enabled form, before send confirmation dialog can be required.
+
+
+#### Add Assets
+
+If you want to start quickly, you can enable Add assets checkbox - and then Add CSS and JS assets.    
+This will include necessary styles (Bootstrap, AJAX, October AJAX) and scripts (jQuery, Bootstrap, October AJAX framework and extras).
+
+But you have to include Twig tags ````{% styles %}```` and ````{% scripts %}```` into your layout or page like this:
+
+````
+<html>
+	<head>
+		{% styles %}
+	</head>
+<body>
+
+	{% page %}
+
+	{% scripts %}
+
+</body>
+
+</html>
+````
+
+If you want to insert assets by hand, you can do it this way (or similar):
+
+````
+<html>
+	<head>
+		<link href="{{['~/modules/system/assets/css/framework.extras.css']|theme }}.css" rel="stylesheet">
+	</head>
+<body>
+
+	{% page %}
+
+	<script type="text/javascript" src="{{ [
+		'@jquery',
+		'@framework',
+		'@framework.extras']|theme}}.js">
+	</script>
+
+</body>
+
+</html>
+````
+
+### SEND BUTTON
+
+* You can set button class and text.
+* Form can automatically redirect to given URL after successful submit.
+
+
+### FIELDS
+
+Here you can add fields to build your contact (or other) form.
+
+The idea is simple (and solution is so I hope):
+
+* Click to add new field
+* Set it's name (this is used for ````<input name="{{name}}" id="{{name}}">````), so it should be lowercase without special characters.
+* Set Label if you need one (it is used for descriptive text above input field)
+* Set autofocus if you want cursor to automatically jump to this field (if checked more than one field, cursor jumps to first one)
+* Add field validation rules:
+ * You can add one or more validation rules and error messages for them
+ * Error messages will be shown above input field
+* You can reorder fields by drag and drop left circle (all fields can be collapsed by pressing Ctrl+click (Cmd+click on MacOS) on arrow in right top corners)
+
+### COLUMNS MAPPING
+
+System writes all form data in database, but for quick overview Name, Email and Message columns are visible separately in Messages list.
+
+But you have to help system to identify these columns by mapping to your form fields.
+
+These mappings are also used for auto reply emails where at least Email field mapping is important.
+
+
+### ANTISPAM
+
+Very simple implementation of passive antispam (inspired by [Nette AntiSpam Control](https://gist.github.com/Michal-Mikolas/2388131)).
+
+The idea behind this is to check how fast is form send and if robots-catching field is filled.
+
+* When allowed, you can set form delay (in seconds) to prevent too fast form sending (mostly by robots). You can add custom error message (will be shown in general error message box above form).
+* You can add antispam field label and error message for non JavaScript enabled browsers.
+ * If JavaScript is working, antispam field is automatically hidden and cleared.
+
+#### Check sender's IP
+
+You can add an extra form protection with limit submits from one IP address.
+
+This check has own error message and custom field to set maximum submits.
+
+
+### EMAIL
+
+Mails can be sent directly or queued ([OctoberCMS queue](https://octobercms.com/docs/services/queues) must be configured!).
+
+Don't forget to configure mail preferences in *Settings > Mail > Mail configuration*!
+
+#### Allow autoreply
+
+Email can be send to form sender as confirmation.
+
+* You have to enter email address and name - it will be used as FROM field
+* Email subject can be manually added here (or edited in *Settings > Mail > Mail templates (code: janvince.smallcontactform::mail.autoreply)*)
+* Email TO address and name have to be assigned to form fields (in selections only corresponding field types are shown - if you don't see one, try to check it's type in Fields tab)
+ * Message field can be also assigned (and will be saved separately into database)
+
+#### Allow notifications
+
+A notification of sent form can be send to provided email address.
+
+
+## TRANSLATION
+
+You can allow translation with [RainLab Translate](https://octobercms.com/plugin/rainlab-translate) plugin.
+
+> After installation of Translate plugin, please add at least two languages in *Settings > Translate > Manage languages*.
+> For translations to work there must be a localePicker component included in your layout/page.
+
+#### Form texts
+
+Most of Small Contact form texts can be edited right in *Settings > Small plugins > Contact form*.
+
+#### Custom form fields
+
+Translate plugin doesn't supports translation of individual repeater fields yet, so form field texts (label, validation error messages) have to be - for now - translated in a dictionary: *Settings > Translate > Translate messages*
+
+> Please note that form fields labels will be shown in dictionary after first form render (on your frontend page) and validation error messages after first send.
+
+#### Email templates
+
+You can create your own email templates in *Settings > Mail > Mail templates* (for hint look inside of default templates starting with *janvince.smallcontactform::*).
+
+Remember your email templates CODE and put in in Small Contact form email settings in *Settings > Small plugins > Contact form > Email tab*. For each language there can be specific template.
+
+There is ````{{fields}}```` array available inside of email templates.
+
+*If your custom form field has name eg. 'email', you use it in template with ````{{fields.email}}````.*
+
+## MESSAGES LIST
+
+All sent data from Contact form are saved and listed in backend Messages list.
+
+If email, name and message fields are assigned on *Settings > Small plugins > contact form > Columns mapping tab*, they will be saved and shown in separate columns.
+
+You can click on a record to see all form data. The message will be marked as read.
+
+
+## DASHBOARD REPORT WIDGETS
+
+There are available report widgets to be used on OctoberCMS dashboard.
+
+#### Messages stats
+
+Shows basic messages statistics.
+
+
+#### New messages
+
+Shows number of new messages. The color changes to green if there are any.
+
+You can simply click widget to open Messages list.
+
+
+----
+> My thanks goes to:    
+> [OctoberCMS](http://www.octobercms.com) team members and supporters for this great system.   
+> [Andrew Measham](https://unsplash.com/@andrewmeasham) for his photo.   
+> [Font Awesome](http://fontawesome.io/icons/) for nice icons.
+
+
+Created by [Jan Vince](http://www.vince.cz), freelance web designer from Czech Republic.
---
+++ plugins/janvince/smallcontactform/components/SmallContactForm.php
@@ -0,0 +1,522 @@
+<?php namespace JanVince\SmallContactForm\Components;
+
+use Cms\Classes\ComponentBase;
+use JanVince\SmallContactForm\Models\Settings;
+use JanVince\SmallContactForm\Models\Message;
+
+use Validator;
+use Illuminate\Support\MessageBag;
+use Redirect;
+use Request;
+use Input;
+use Session;
+use Flash;
+use Form;
+use Log;
+use App;
+
+class SmallContactForm extends ComponentBase
+{
+
+  // public $implement = [
+    //     '@RainLab.Translate.Behaviors.TranslatableModel',
+    // ];
+  //
+  // public $translatable = [];
+
+  private $validationRules;
+  private $validationMessages;
+
+  private $postData = [];
+  private $post;
+
+  private $errorAutofocus;
+
+
+    public function componentDetails()
+    {
+        return [
+            'name'        => 'janvince.smallcontactform::lang.controller.contact_form.name',
+            'description' => 'janvince.smallcontactform::lang.controller.contact_form.description'
+        ];
+    }
+
+  public function onRun(){
+
+    if ( Session::get('flashSuccess') ) {
+
+      $this->page['flashSuccess'] = Session::get('flashSuccess', $this->alias);
+
+    }
+
+
+    // Inject CSS assets if required
+    if(Settings::getTranslated('add_assets') && Settings::getTranslated('add_css_assets')){
+      $this->addCss('/modules/system/assets/css/framework.extras.css');
+      $this->addCss('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css');
+    }
+
+    // Inject JS assets if required
+    if(Settings::getTranslated('add_assets') && Settings::getTranslated('add_js_assets')){
+      $this->addJs('https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js');
+      $this->addJs('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js');
+      $this->addJs('/modules/system/assets/js/framework.js');
+      $this->addJs('/modules/system/assets/js/framework.extras.js');
+    }
+
+  }
+
+  /**
+   * Form handler
+   */
+  public function onFormSend(){
+    /**
+     * Validation
+     */
+    $this->setFieldsValidationRules();
+    $errors = [];
+
+    $this->post = Input::all();
+
+    // IP protection is enabled (has highest priority)
+    if( Settings::getTranslated('add_ip_protection') ) {
+
+      $max = ( Settings::getTranslated('add_ip_protection_count') ? intval(Settings::getTranslated('add_ip_protection_count')) : intval(e(trans('janvince.smallcontactform::lang.settings.antispam.add_ip_protection_count_placeholder'))) );
+
+      if( empty($max) ) {
+        $max = 3;
+      }
+
+      $currentIp = Request::ip();
+
+      if( empty($currentIp) ) {
+        Log::error('SMALL CONTACT FORM ERROR: Could not get remote IP address!');
+        $errors[] = e(trans('janvince.smallcontactform::lang.settings.antispam.add_ip_protection_error_get_ip'));
+      } else {
+
+        $message = new Message;
+
+        if($message->testIPAddress($currentIp) >= $max) {
+          $errors[] = ( Settings::getTranslated('add_ip_protection_error_too_many_submits') ? Settings::getTranslated('add_ip_protection_error_too_many_submits') : e(trans('janvince.smallcontactform::lang.settings.antispam.add_ip_protection_error_too_many_submits_placeholder')) );
+        }
+
+      }
+
+    }
+
+    // Antispam validation if allowed
+    if( Settings::getTranslated('add_antispam') ) {
+      $this->validationRules[('_protect-' . $this->alias)] = 'size:0';
+
+      if( !empty($this->post['_form_created']) ) {
+
+        $delay = ( Settings::getTranslated('antispam_delay') ? intval(Settings::getTranslated('antispam_delay')) : intval(e(trans('janvince.smallcontactform::lang.settings.antispam.antispam_delay_placeholder'))) );
+
+        if(!$delay) {
+          $delay = 5;
+        }
+
+        $formCreatedTime = strtr(Input::get('_form_created'), 'jihgfedcba', '0123456789');
+
+        $this->post['_form_created'] = intval($formCreatedTime + $delay);
+
+        $this->validationRules['_form_created'] = 'numeric|max:' . time();
+
+      }
+
+    }
+
+    // Validate
+    $validator = Validator::make($this->post, $this->validationRules, $this->validationMessages);
+    $validator->valid();
+    $this->validationMessages = $validator->messages();
+    $this->setPostData($validator->messages());
+
+    if($validator->invalid() or !empty($errors)){
+
+      // Form main error msg
+      $errors[] = ( Settings::getTranslated('form_error_msg') ? Settings::getTranslated('form_error_msg') : e(trans('janvince.smallcontactform::lang.settings.form.error_msg_placeholder')));
+
+      // validation error msg for Antispam field
+      if( empty($this->postData[('_protect' . $this->alias)]['error']) && !empty($this->postData['_form_created']['error']) ) {
+        $errors[] = ( Settings::getTranslated('antispam_delay_error_msg') ? Settings::getTranslated('antispam_delay_error_msg') : e(trans('janvince.smallcontactform::lang.settings.antispam.antispam_delay_error_msg_placeholder')));
+      }
+
+      Flash::error(implode(PHP_EOL, $errors));
+
+    } else {
+
+      Flash::success(
+        ( Settings::getTranslated('form_success_msg') ? Settings::getTranslated('form_success_msg') : e(trans('janvince.smallcontactform::lang.settings.form.success_msg_placeholder')) )
+      );
+
+      Session::flash('flashSuccess', $this->alias);
+
+      $message = new Message;
+
+      // Store data in DB
+      $message->storeFormData($this->postData);
+
+      // Send auto reply
+      $message->sendAutoreplyEmail($this->postData);
+
+      // Send notification
+      $message->sendNotificationEmail($this->postData);
+
+      // Redirect to prevent repeated sending of form
+      // Clear data after success AJAX send
+      if(!Request::ajax()){
+        if( Settings::getTranslated('allow_redirect') and !empty(Settings::getTranslated('redirect_url')) ) {
+
+          if( !empty(Settings::getTranslated('redirect_url_external')) ) {
+            $path = Settings::getTranslated('redirect_url');
+          } else {
+            $path = url(Settings::getTranslated('redirect_url'));
+          }
+
+          return Redirect::to($path);
+
+        } else {
+          return Redirect::refresh();
+        }
+
+      } else {
+        $this->post = [];
+        $this->postData = [];
+        $this->page['flashSuccess'] = $this->alias;
+      }
+
+    }
+
+  }
+
+  /**
+   * Get plugin settings
+   * Twig access: contactForm.fields
+   * @return array
+   */
+  public function fields(){
+
+    return Settings::getTranslated('form_fields', []);
+
+  }
+
+  /**
+   * Get form attributes
+   */
+  public function getFormAttributes(){
+
+    $attributes = [];
+
+    $attributes['class'] = Settings::getTranslated('form_css_class');
+    $attributes['request'] = $this->alias . '::onFormSend';
+    $attributes['method'] = 'POST';
+
+    if( Settings::getTranslated('form_allow_ajax', 0) ) {
+
+      $attributes['data-request'] = $this->alias . '::onFormSend';
+      $attributes['data-request-validate'] = NULL;
+      $attributes['data-request-update'] = "'". $this->alias ."::scf-message':'#scf-message-". $this->alias ."','". $this->alias ."::scf-form':'#scf-form-". $this->alias ."'";
+
+    }
+
+    if( Settings::getTranslated('allow_redirect') and !empty(Settings::getTranslated('redirect_url')) ) {
+
+      if( !empty(Settings::getTranslated('redirect_url_external')) ) {
+        $attributes['data-request-redirect'] = Settings::getTranslated('redirect_url');
+      } else {
+        $attributes['data-request-redirect'] = url(Settings::getTranslated('redirect_url'));
+      }
+
+    }
+
+    if( Settings::getTranslated('form_send_confirm_msg') and Settings::getTranslated('form_allow_confirm_msg') ) {
+
+      $attributes['data-request-confirm'] = Settings::getTranslated('form_send_confirm_msg');
+
+    }
+
+    return $attributes;
+
+  }
+
+  /**
+   * Generate field HTML code
+   * @return string
+   */
+  public function getFieldHtmlCode(array $fieldSettings){
+
+    if(empty($fieldSettings['name']) && empty($fieldSettings['type'])){
+      return NULL;
+    }
+
+    $fieldType = Settings::getFieldTypes($fieldSettings['type']);
+    $fieldRequired = $this->isFieldRequired($fieldSettings);
+
+    $output = [];
+
+    $wrapperCss = ( $fieldSettings['wrapper_css'] ? $fieldSettings['wrapper_css'] : $fieldType['wrapper_class'] );
+
+    // Add wrapper error class if there are any
+    if(!empty($this->postData[$fieldSettings['name']]['error'])){
+      $wrapperCss .= ' has-error';
+    }
+
+    $output[] = '<div class="' . $wrapperCss . '">';
+
+      // Label classic
+      if( !empty($fieldSettings['label']) and !Settings::getTranslated('form_use_placeholders') and !empty($fieldType['label']) ){
+        $output[] = '<label class="control-label ' . ( $fieldRequired ? 'required' : '' ) . '" for="' . $fieldSettings['name'] . '">' . Settings::getDictionaryTranslated($fieldSettings['label']) . '</label>';
+      }
+
+      // Label as container
+      if( empty($fieldType['label']) ){
+        $output[] = '<label>';
+      }
+
+
+      // Add help-block if there are errors
+      if(!empty($this->postData[$fieldSettings['name']]['error'])){
+        $output[] = '<small class="help-block">' . Settings::getDictionaryTranslated($this->postData[$fieldSettings['name']]['error']) . "</small>";
+      }
+
+      // Field attributes
+      $attributes = [
+        'id' => $fieldSettings['name'],
+        'name' => $fieldSettings['name'],
+        'class' => ($fieldSettings['field_css'] ? $fieldSettings['field_css'] : $fieldType['field_class'] ),
+        'value' => (!empty($this->postData[$fieldSettings['name']]['value']) && empty($fieldType['html_close']) ? $this->postData[$fieldSettings['name']]['value'] : '' ),
+      ];
+
+      // Placeholders if enabled
+      if(Settings::getTranslated('form_use_placeholders')){
+        $attributes['placeholder'] = Settings::getDictionaryTranslated($fieldSettings['label']);
+      }
+
+
+      // Autofocus only when no error
+      if(!empty($fieldSettings['autofocus']) && !Flash::error()){
+        $attributes['autofocus'] = NULL;
+      }
+
+      // Add custom attributes from field settings
+      if(!empty($fieldType['attributes'])){
+        $attributes = array_merge($attributes, $fieldType['attributes']);
+      }
+
+      // Add error class if there are any and autofocus field
+      if(!empty($this->postData[$fieldSettings['name']]['error'])){
+        $attributes['class'] = $attributes['class'] . ' error';
+
+        if(empty($this->errorAutofocus)){
+          $attributes['autofocus'] = NULL;
+          $this->errorAutofocus = true;
+        }
+
+      }
+
+      if($fieldRequired){
+        $attributes['required'] = NULL;
+      }
+
+
+      $output[] = '<' . $fieldType['html_open'] . ' ' . $this->formatAttributes($attributes) . '>';
+
+      // For pair tags insert value between
+      if(!empty($this->postData[$fieldSettings['name']]['value']) && !empty($fieldType['html_close'])){
+        $output[] = $this->postData[$fieldSettings['name']]['value'];
+      }
+
+      // For tags withou label put text inline
+      if( empty( $fieldType['label'] ) ){
+        $output[] = Settings::getDictionaryTranslated($fieldSettings['label']);
+      }
+
+      if(!empty($fieldType['html_close'])){
+        $output[] = '</' . $fieldType['html_close'] . '>';
+      }
+
+      // Label as container
+      if( empty($fieldType['label']) ){
+        $output[] = '</label>';
+      }
+
+    $output[] = "</div>";
+
+    return(implode('', $output));
+
+  }
+
+  /**
+   * Generate antispam field HTML code
+   * @return string
+   */
+  public function getAntispamFieldHtmlCode(){
+
+    if( !Settings::getTranslated('add_antispam') ){
+      return NULL;
+    }
+
+    $output = [];
+
+    $output[] = '<div id="_protect-wrapper-' . $this->alias . '" class="form-group _protect-wrapper' . (Input::get('_protect-'.$this->alias) ? 'has-error' : '') . '">';
+
+      $output[] = '<label class="control-label">' . ( Settings::getTranslated('antispam_label') ? Settings::getTranslated('antispam_label') : e(trans('janvince.smallcontactform::lang.settings.antispam.antispam_label_placeholder'))  ) . '</label>';
+
+      $output[] = '<input type="hidden" name="_form_created" value="' . strtr(time(), '0123456789', 'jihgfedcba') . '">';
+
+      // Add help-block if there are errors
+      if(!empty($this->postData[('_protect'.$this->alias)]['error'])){
+        $output[] = '<small class="help-block">' . ( Settings::getTranslated('antispam_error_msg') ? Settings::getTranslated('antispam_error_msg') : e(trans('janvince.smallcontactform::lang.settings.antispam.antispam_error_msg_placeholder'))  ) . "</small>";
+      }
+
+      // Field attributes
+      $attributes = [
+        'id' => '_protect-'.$this->alias,
+        'name' => '_protect',
+        'class' => '_protect form-control',
+        'value' => 'http://',
+      ];
+
+      // Add error class if field is not empty
+      if( Input::get('_protect-'.$this->alias) ){
+        $attributes['class'] = $attributes['class'] . ' error';
+
+        if(empty($this->errorAutofocus)){
+          $attributes['autofocus'] = NULL;
+          $this->errorAutofocus = true;
+        }
+
+      }
+
+      $output[] = '<input ' . $this->formatAttributes($attributes) . '>';
+
+    $output[] = "</div>";
+
+    $output[] = "
+      <script>
+        document.getElementById('_protect-" . $this->alias . "').setAttribute('value', '');
+        document.getElementById('_protect-wrapper-" . $this->alias . "').style.display = 'none';
+      </script>
+    ";
+
+    return(implode('', $output));
+
+  }
+
+
+  /**
+   * Generate antispam field HTML code
+   * @return string
+   */
+  public function getSubmitButtonHtmlCode(){
+
+    if( !count($this->fields()) ){
+      return e(trans('janvince.smallcontactform::lang.controller.contact_form.no_fields'));
+    }
+
+    $output = [];
+
+    $wrapperCss = ( Settings::getTranslated('send_btn_wrapper_css') ? Settings::getTranslated('send_btn_wrapper_css') : e(trans('janvince.smallcontactform::lang.settings.buttons.send_btn_wrapper_css_placeholder')) );
+
+    $output[] = '<div id="submit-wrapper" class="' . $wrapperCss . '">';
+
+      $output[] = '<button type="submit" data-attach-loading class="oc-loader ' . ( Settings::getTranslated('send_btn_css_class') ? Settings::getTranslated('send_btn_css_class') : e(trans('janvince.smallcontactform::lang.settings.buttons.send_btn_css_class_placeholder')) ) . '">';
+
+      $output[] = ( Settings::getTranslated('send_btn_text') ? Settings::getTranslated('send_btn_text') : e(trans('janvince.smallcontactform::lang.settings.buttons.send_btn_text_placeholder')) );
+
+      $output[] = '</button>';
+
+    $output[] = "</div>";
+
+    return(implode('', $output));
+
+  }
+
+
+  /**
+   * Generate validation rules and messages
+   */
+  private function setFieldsValidationRules(){
+
+    $fieldsDefinition = $this->fields();
+
+    $validationRules = [];
+    $validationMessages = [];
+
+    foreach($fieldsDefinition as $field){
+
+      if(!empty($field['validation'])) {
+        $rules = [];
+
+        foreach($field['validation'] as $rule) {
+          $rules[] = $rule['validation_type'];
+
+          if(!empty($rule['validation_error'])){
+            $validationMessages[($field['name'] . '.' . $rule['validation_type'] )] = Settings::getDictionaryTranslated($rule['validation_error']);
+          }
+        }
+        $validationRules[$field['name']] = implode('|', $rules);
+      }
+
+    }
+
+    $this->validationRules = $validationRules;
+    $this->validationMessages = $validationMessages;
+
+  }
+
+
+  /**
+   * Generate post data with errors
+   */
+  private function setPostData(MessageBag $validatorMessages){
+
+    foreach( Input::all() as $key => $value){
+
+      $this->postData[$key] = [
+        'value' => e(Input::get($key)),
+        'error' => $validatorMessages->first($key),
+      ];
+
+    }
+
+  }
+
+  /**
+   * Format attributes array
+   * @return array
+   */
+  private function formatAttributes(array $attributes) {
+
+    $output = [];
+
+    foreach ($attributes as $key => $value) {
+      $output[] = $key . '="' . $value . '"';
+    }
+
+    return implode(' ', $output);
+
+  }
+
+  /**
+   * Search for required validation type
+   */
+  private function isFieldRequired($fieldSettings){
+
+    if(empty($fieldSettings['validation'])){
+      return false;
+    }
+
+    foreach($fieldSettings['validation'] as $rule) {
+      if(!empty($rule['validation_type']) && $rule['validation_type'] == 'required'){
+        return true;
+      }
+    }
+
+    return false;
+
+  }
+
+
+}
