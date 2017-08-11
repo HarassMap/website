bc413a1b697302231e86f5af3d784e4b7301d621
Paul Barton <info@thisispaul.co.uk>
2017-08-11 19:37:44 +0100

    Adding json colours data to the domain

---
+++ plugins/harassmap/incidents/updates/builder_table_update_harassmap_incidents_domain_11.php
@@ -0,0 +1,23 @@
+<?php namespace Harassmap\Incidents\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateHarassmapIncidentsDomain11 extends Migration
+{
+    public function up()
+    {
+        Schema::table('harassmap_incidents_domain', function($table)
+        {
+            $table->json('colours')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('harassmap_incidents_domain', function($table)
+        {
+            $table->dropColumn('colours');
+        });
+    }
+}
