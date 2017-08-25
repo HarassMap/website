6362d3eda17963ec4bf3c516ed34f4dd9547cdd7
Paul Barton <info@thisispaul.co.uk>
2017-08-25 18:26:50 +0100

    [master] Adding help to the mail layouts

--- plugins/harassmap/mail/lang/en/lang.php
+++ plugins/harassmap/mail/lang/en/lang.php
@@ -2,5 +2,10 @@
     'plugin' => [
         'name' => 'HarassMap Mail',
         'description' => ''
+    ],
+    'form' => [
+        'layout' => [
+            'twig_help' => 'The layout can use two tags: {{ content|raw }} to output the email content and  {{ css|raw }} to output the styles'
+        ]
     ]
 ];
