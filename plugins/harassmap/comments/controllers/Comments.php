79a18583a2a9b6fcaa6d256f11c0e704211dd978
Paul Barton <info@thisispaul.co.uk>
2017-08-27 17:49:54 +0100

    [master] Adding domain to the comments list

--- plugins/harassmap/comments/controllers/Comments.php
+++ plugins/harassmap/comments/controllers/Comments.php
@@ -2,6 +2,7 @@
 
 use Backend\Classes\Controller;
 use BackendMenu;
+use BackendAuth;
 
 class Comments extends Controller
 {
@@ -19,5 +20,41 @@ class Comments extends Controller
     public function formExtendQuery($query)
     {
         $query->withTrashed();
+    }
+
+    /**
+     * @var string
+     *
+     * public $domain_id = 'id;
+     */
+
+    public function listExtendQuery($query)
+    {
+        $user = BackendAuth::getUser();
+
+        // if the user is a super use then stop here
+        if ($user->isSuperUser() || $user->hasPermission(['harassmap.incidents.domain.manage_domains'])) {
+            return;
+        }
+
+        $domains = $user->domains;
+
+        if ($domains->isEmpty()) {
+            $query->where('id', '=', -1);
+        }
+
+        $domain_ids = [];
+
+        foreach ($domains as $domain) {
+            $domain_ids[] = $domain->id;
+        }
+
+        $query->whereHas('topic', function ($query) use ($domain_ids) {
+            $query->whereHas('incident', function($query) use ($domain_ids) {
+                $query->whereIn('domain_id', $domain_ids);
+            });
+        });
+
+
     }
 }
