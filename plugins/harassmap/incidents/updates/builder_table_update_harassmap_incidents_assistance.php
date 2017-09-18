e743fd1912ea88bf7f2ef1e862652e8a94a5b686
Paul Barton <info@thisispaul.co.uk>
2017-09-18 15:37:04 +0100

    Adding database updates

---
+++ plugins/harassmap/incidents/updates/builder_table_update_harassmap_incidents_assistance.php
@@ -0,0 +1,25 @@
+<?php namespace Harassmap\Incidents\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateHarassmapIncidentsAssistance extends Migration
+{
+    public function up()
+    {
+        Schema::table('harassmap_incidents_assistance', function($table)
+        {
+            $table->integer('domain_id')->unsigned();
+            $table->foreign('domain_id')->references('id')->on('harassmap_incidents_domain');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('harassmap_incidents_assistance', function($table)
+        {
+            $table->dropForeign('harassmap_incidents_assistance_domain_id_foreign');
+            $table->dropColumn('domain_id');
+        });
+    }
+}
--- plugins/harassmap/incidents/updates/builder_table_update_harassmap_incidents_role.php
+++ plugins/harassmap/incidents/updates/builder_table_update_harassmap_incidents_role.php
@@ -1,23 +1,27 @@
-<?php namespace Harassmap\Incidents\Updates;
+<?php
 
-use Schema;
+namespace Harassmap\Incidents\Updates;
+
 use October\Rain\Database\Updates\Migration;
+use Schema;
 
 class BuilderTableUpdateHarassmapIncidentsRole extends Migration
 {
     public function up()
     {
-        Schema::table('harassmap_incidents_role', function($table)
-        {
+        Schema::table('harassmap_incidents_role', function ($table) {
             $table->string('name', 100)->change();
+            $table->integer('domain_id')->unsigned();
+            $table->foreign('domain_id')->references('id')->on('harassmap_incidents_domain');
         });
     }
-    
+
     public function down()
     {
-        Schema::table('harassmap_incidents_role', function($table)
-        {
+        Schema::table('harassmap_incidents_role', function ($table) {
             $table->string('name', 255)->change();
+            $table->dropForeign('harassmap_incidents_role_domain_id_foreign');
+            $table->dropColumn('domain_id');
         });
     }
 }
