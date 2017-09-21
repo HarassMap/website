858a7093734255d038e7d5152a99b31d4b8beec6
Paul Barton <info@thisispaul.co.uk>
2017-09-21 16:01:27 +0100

    Adding our own version of the mailchimp plugin

---
+++ plugins/harassmap/mailchimp/updates/builder_table_update_harassmap_incidents_domain.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Harassmap\MailChimp\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateHarassmapIncidentsDomain extends Migration
+{
+    public function up()
+    {
+        Schema::table('harassmap_incidents_domain', function($table)
+        {
+            $table->string('mailchimp_api')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('harassmap_incidents_domain', function($table)
+        {
+            $table->dropColumn('mailchimp_api_key');
+        });
+    }
+}
