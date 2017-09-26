db28748d28ab34f8ae076790260b6f8ddade5f25
Paul Barton <info@thisispaul.co.uk>
2017-09-26 18:55:28 +0100

    Removing mailer test

--- plugins/harassmap/incidents/routes.php
+++ plugins/harassmap/incidents/routes.php
@@ -1,12 +1,6 @@
 <?php
 
-use Harassmap\Incidents\Classes\Mailer;
-
 Route::get('api/incidents', [
     'as' => 'incidents',
     'uses' => 'Harassmap\Incidents\Classes\Api\IncidentsController@index'
 ]);
-
-Route::get('mail/test', function () {
-    Mailer::sendNotificationMail();
-});
