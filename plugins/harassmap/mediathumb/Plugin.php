68b03b8c3881067d3f590f21b215e4bfc39a21f3
Paul Barton <info@thisispaul.co.uk>
2017-09-28 20:46:30 +0100

    Missed files whoops

---
+++ plugins/harassmap/mediathumb/Plugin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Harassmap\Mediathumb;
+
+use Harassmap\Mediathumb\Classes\MediaThumb;
+use System\Classes\PluginBase;
+
+class Plugin extends PluginBase
+{
+
+    public function registerMarkupTags()
+    {
+        return [
+            'filters' => [
+                'mediathumb_resize' => [$this, 'mediathumb_resize']
+            ]
+        ];
+    }
+
+    public function mediathumb_resize($img, $mode = null, $size = null, $quality = null)
+    {
+        return MediaThumb::resize($img, $mode, $size, $quality);
+    }
+}
