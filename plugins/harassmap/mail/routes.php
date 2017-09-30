33b9d1e0216b304140b1de601838c4a998db74f1
Paul Barton <info@thisispaul.co.uk>
2017-09-30 15:09:39 +0100

    Only saving correct locales to the user

--- plugins/harassmap/mail/routes.php
+++ plugins/harassmap/mail/routes.php
@@ -2,6 +2,7 @@
 
 use RainLab\Translate\Classes\Translator;
 use RainLab\User\Facades\Auth;
+use RainLab\Translate\Models\Locale;
 
 App::before(function ($request) {
 
@@ -10,14 +11,15 @@ App::before(function ($request) {
     }
 
     $translator = Translator::instance();
-    if (!$translator->isConfigured())
+    if (!$translator->isConfigured()) {
         return;
+    }
 
     $locale = Request::segment(1);
     $user = Auth::getUser();
 
     // if there is a user
-    if ($locale && $user && $user->locale !== $locale) {
+    if ($locale && Locale::isValid($locale) && $user && $user->locale !== $locale) {
         $user->locale = $locale;
         $user->save();
     }
