a69f50cbcd708108fab867e392612903e60b30e7
Paul Barton <info@thisispaul.co.uk>
2017-10-12 16:50:56 +0100

    Adding lang link to sitemap

--- plugins/harassmap/incidents/models/Logo.php
+++ plugins/harassmap/incidents/models/Logo.php
@@ -9,6 +9,20 @@ use RainLab\Translate\Models\Locale;
 
 /**
  * Model
+ *
+ * @property int $id
+ * @property int $domain_id
+ * @property string $language
+ * @property string $position
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\Harassmap\Incidents\Models\Logo whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Harassmap\Incidents\Models\Logo whereDomainId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Harassmap\Incidents\Models\Logo whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Harassmap\Incidents\Models\Logo whereLanguage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Harassmap\Incidents\Models\Logo wherePosition($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Harassmap\Incidents\Models\Logo whereUpdatedAt($value)
+ * @mixin \Eloquent
  */
 class Logo extends Model
 {
--- plugins/harassmap/sitemap/classes/Definition.php
+++ plugins/harassmap/sitemap/classes/Definition.php
@@ -2,13 +2,32 @@
 
 namespace Harassmap\Sitemap\Classes;
 
+use Cms\Classes\Page;
+use DOMDocument;
 use Harassmap\Incidents\Models\Domain;
 use October\Rain\Support\Traits\Singleton;
-use DOMDocument;
+use RainLab\Translate\Classes\Translator;
+use RainLab\Translate\Models\Locale;
+use Route;
 
 class Definition
 {
     use Singleton;
+
+    /**
+     * Maximum URLs allowed (Protocol limit is 50k)
+     */
+    const MAX_URLS = 50000;
+
+    /**
+     * Maximum generated URLs per type
+     */
+    const MAX_GENERATED = 10000;
+
+    /**
+     * @var integer A tally of URLs added to the sitemap
+     */
+    protected $urlCount = 0;
 
     /**
      * @var DOMDocument element
@@ -20,16 +39,68 @@ class Definition
      */
     protected $xmlObject;
 
+    protected $pages = [
+        'home',
+        'contact',
+        'report/incidents',
+        'report/intervention',
+        'chart/index',
+        'news/index',
+        'reports/index',
+        'tips/index',
+    ];
+
     public function generateSitemap()
     {
         $domain = Domain::getBestMatchingDomain();
+        $languages = explode(',', $domain->languages);
 
+        // get the default locale
+        $defaultLocale = $domain->default_language;
+        if (!Locale::isValid($defaultLocale)) {
+            $defaultLocale = 'en';
+        }
+
+        $translator = Translator::instance();
+        if (!$translator->isConfigured()) {
+            return;
+        }
+
+        $list = Page::sortBy('baseFileName')->all();
+
+        // get every cms page
+        foreach ($list as $page) {
+            if (in_array($page->getBaseFileName(), $this->pages)) {
+
+                $this->forceRouterLocale($defaultLocale);
+
+                $url = Page::url($page->getBaseFileName());
+                $alternate = [];
+
+                // generate urls for all the alternate languages
+                foreach ($languages as $language) {
+                    if($language !== $defaultLocale) {
+                        $this->forceRouterLocale($language);
+                        $alternate[$language] = Page::url($page->getBaseFileName());
+                    }
+                }
+
+                $this->addItemToSet($url, $alternate, $page->mtime);
+            }
+        }
 
         $urlSet = $this->makeUrlSet();
         $xml = $this->makeXmlObject();
         $xml->appendChild($urlSet);
 
         return $xml->saveXML();
+    }
+
+    protected function forceRouterLocale($locale)
+    {
+        Route::group(['prefix' => $locale], function () {
+            Route::any('{slug}', 'Cms\Classes\CmsController@run')->where('slug', '(.*)?');
+        });
     }
 
     protected function makeXmlObject()
@@ -53,10 +124,59 @@ class Definition
         $xml = $this->makeXmlObject();
         $urlSet = $xml->createElement('urlset');
         $urlSet->setAttribute('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
-        $urlSet->setAttribute('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
-        $urlSet->setAttribute('xsi:schemaLocation', 'http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd');
+        $urlSet->setAttribute('xmlns:xhtml', 'http://www.w3.org/1999/xhtml');
 
         return $this->urlSet = $urlSet;
+    }
+
+    protected function addItemToSet($url, $languages = [], $mtime = null, $changefreq = 'weekly', $priority = 0.5)
+    {
+        if ($mtime instanceof \DateTime) {
+            $mtime = $mtime->getTimestamp();
+        }
+
+        $xml = $this->makeXmlObject();
+        $urlSet = $this->makeUrlSet();
+        $mtime = $mtime ? date('c', $mtime) : date('c');
+
+        $urlElement = $this->makeUrlElement(
+            $xml,
+            $url,
+            $languages,
+            $mtime,
+            $changefreq,
+            $priority
+        );
+
+        if ($urlElement) {
+            $urlSet->appendChild($urlElement);
+        }
+
+        return $urlSet;
+    }
+
+    protected function makeUrlElement($xml, $pageUrl, $languages, $lastModified, $frequency, $priority)
+    {
+        if ($this->urlCount >= self::MAX_URLS) {
+            return false;
+        }
+
+        $this->urlCount++;
+
+        $url = $xml->createElement('url');
+        $url->appendChild($xml->createElement('loc', $pageUrl));
+        $url->appendChild($xml->createElement('lastmod', $lastModified));
+        $url->appendChild($xml->createElement('changefreq', $frequency));
+        $url->appendChild($xml->createElement('priority', $priority));
+
+        foreach($languages as $code => $path) {
+            $link = $url->appendChild($xml->createElement('xhtml:link'));
+            $link->setAttribute('rel', 'alternate');
+            $link->setAttribute('hreflang', $code);
+            $link->setAttribute('href', $path);
+        }
+
+        return $url;
     }
 
 }
