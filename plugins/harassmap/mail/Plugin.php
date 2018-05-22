7b07741dc59689431ebb618dc76145723128c9f9
Ijeoma Arisah <ijeomarisah@gmail.com>
2018-05-22 18:58:38 +0100

    8383 add debugger to mail template

--- plugins/harassmap/mail/Plugin.php
+++ plugins/harassmap/mail/Plugin.php
@@ -12,6 +12,10 @@ class Plugin extends PluginBase
     public function register()
     {
         Event::listen('mailer.beforeAddContent', function($mailer, $message, $view, $data) {
+            Logger::info($mailer);
+            Logger::info($message);
+            Logger::info($view);
+            Logger::info($data);
             MailTemplate::addContentToMailer($message, $view, $data);
             return false;
         });
